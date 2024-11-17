a053c2a7b290560f76e963a8e0a3e797376243b4
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2024-11-18 04:15:56 +0500

    sidebar style

--- src/Sidebar/index.jsx
+++ src/Sidebar/index.jsx
@@ -8,27 +8,28 @@ import SidebarIcon5 from "../assets/icons5-Message.png";
 import SidebarIcon6 from "../assets/isons6-Notification.png";
 import SidebarIcon7 from "../assets/icons7-cart-outline.png";
 import SidebarIcon8 from "../assets/icons8_account.png";
+import { useState } from "react";
 
 function Sidebar({ handleHSHowCart, cartLength }) {
   return (
     <div className="sidebar-container">
-      <img src={SidebarIcon1} alt="img" className="home" />
-      <div className="home-icon-home">
-        <div className="icon-home">
-          <img src={SidebarIcon2} alt="img" className="home" />
+      <img src={SidebarIcon1} alt="img" className="icon" />
+      <div className="sidebar-home">
+        <div className="sidebar-home__icon">
+          <img src={SidebarIcon2} alt="img" className="icon" />
         </div>
       </div>
-      <img src={SidebarIcon3} alt="img" className="home" />
-      <img src={SidebarIcon4} alt="img" className="home" />
-      <img src={SidebarIcon5} alt="img" className="home" />
-      <img src={SidebarIcon6} alt="img" className="home" />
-      <div className="home-icon__market">
-        <button onClick={handleHSHowCart}>
-          <img src={SidebarIcon7} alt="img" className="home" />
+      <img src={SidebarIcon3} alt="img" className="icon" />
+      <img src={SidebarIcon4} alt="img" className="icon" />
+      <img src={SidebarIcon5} alt="img" className="icon" />
+      <img src={SidebarIcon6} alt="img" className="icon" />
+      <div className="sidebar-market">
+        <button onClick={handleHSHowCart} className="sidebar-market__button">
+          <img src={SidebarIcon7} alt="img" className="icon" />
         </button>
         {cartLength > 0 && <span className="cart-count">{cartLength}</span>}
       </div>
-      <img src={SidebarIcon8} alt="img" className="home" />
+      <img src={SidebarIcon8} alt="img" className="icon" />
     </div>
   );
 }
