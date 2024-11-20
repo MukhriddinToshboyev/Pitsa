56bbc35ccbccf56ef5e90df19fa1c97e430286e6
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2024-11-21 00:59:01 +0500

    information css

--- src/Menu/Information/index.jsx
+++ src/Menu/Information/index.jsx
@@ -19,14 +19,61 @@ function Information() {
         <nav className="information-navigation">
           <ul className="information-navigation__items">
             <li className="information-navigation__item ">
-              <a href="#">Information</a>
+              <a href="#">
+                <img
+                  src={InformationIcon1}
+                  alt="icon"
+                  className="information-navigation__icon"
+                />
+                +998 71 000 00 71
+              </a>
             </li>
             <li className="information-navigation__item">
-              <a href="#">Language</a>
+              <a href="#">
+                <img
+                  src={InformationIcon2}
+                  alt="icon"
+                  className="information-navigation__icon"
+                />
+                Tashkent, str. Amir Temur 15
+              </a>
             </li>
             <li className="information-navigation__item">
-              <a href="#">Order history</a>
+              <a href="#">
+                <img
+                  src={InformationIcon3}
+                  alt="icon"
+                  className="information-navigation__icon"
+                />
+                F.P.D_77@gmail.com
+              </a>
             </li>
+            <li className="information-navigation__item">
+              <a href="#">
+                <img
+                  src={InformationIcon4}
+                  alt="icon"
+                  className="information-navigation__icon"
+                />
+                www.F.P.D_77.com
+              </a>
+            </li>
+            <div className="information-navigation__item">
+              <ul className="information-navigation__group">
+                <li className="information-navigation__group-icon">
+                  <img src={InformationIcon5} alt="icon" />
+                </li>
+                <li className="information-navigation__group-icon">
+                  <img src={InformationIcon6} alt="icon" />
+                </li>
+                <li className="information-navigation__group-icon">
+                  <img src={InformationIcon7} alt="icon" />
+                </li>
+                <li className="information-navigation__group-icon">
+                  <img src={InformationIcon8} alt="icon" />
+                </li>
+              </ul>
+            </div>
           </ul>
         </nav>
       </div>
