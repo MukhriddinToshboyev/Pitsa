32905e2e8a609a5e9e5133c8626eaa80d90af5c4
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2024-11-20 21:33:23 +0500

    account new code

--- src/Account/index.jsx
+++ src/Account/index.jsx
@@ -7,7 +7,21 @@ function Account() {
 
   return (
     <div className="account-container">
-      <div className="account_order"></div>
+      <div className="account-order">
+        <header className="account-header">
+          <h2 className="account-header__text">Profil</h2>
+        </header>
+        <nav className="account-navigation">
+          <div className="account-navigation__ellipse">
+            <h2 className="account-navigation__ellipse-text">D</h2>
+          </div>
+          <h2 className="account-navigation__text">Denis</h2>
+        </nav>
+        <section className="account-section">
+          <img src="" alt="" className="account-section__icon" />
+          <h2 className="account-section__text">Order history</h2>
+        </section>
+      </div>
     </div>
   );
 }
