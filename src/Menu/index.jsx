68aab0989da439e17bee6dd4defbe4682c937711
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2024-11-20 23:46:03 +0500

    Menu new icon

--- src/Menu/index.jsx
+++ src/Menu/index.jsx
@@ -1,22 +1,54 @@
 import "./Menu.css";
 
+import MenuIcon1 from "../assets/Menu-icons8_info_64px 1.svg";
+import MenuIcon2 from "../assets/Menu-icons8_Earth_Globe_32px 1.svg";
+import MenuIcon3 from "../assets/Menu-icons8_clock_100px 1 (1).svg";
+import { useState } from "react";
+
 function Menu() {
+  const [information, setInformation] = useState(false);
+
   return (
     <div className="menu-container">
-      <div className="menu-order">
+      {/* {information && } */}
+      <div className="menu-function">
         <header className="menu-header">
           <h2 className="menu-header__text"> Functions</h2>
         </header>
         <nav className="menu-navigation">
           <ul className="menu-navigation__items">
-            <li className="menu-navigation__item orders-one">
-              <a href="#">Information</a>
+            <li
+              onClick={() => setInformation(true)}
+              className="menu-navigation__item information"
+            >
+              <a href="#">
+                <img
+                  src={MenuIcon1}
+                  alt="icon"
+                  className="menu-navigation__icon"
+                />
+                Information
+              </a>
             </li>
             <li className="menu-navigation__item">
-              <a href="#">Language</a>
+              <a href="#">
+                <img
+                  src={MenuIcon2}
+                  alt="icon"
+                  className="menu-navigation__icon"
+                />
+                Language
+              </a>
             </li>
             <li className="menu-navigation__item">
-              <a href="#">Order history</a>
+              <a href="#">
+                <img
+                  src={MenuIcon3}
+                  alt="icon"
+                  className="menu-navigation__icon"
+                />
+                Order history
+              </a>
             </li>
           </ul>
         </nav>
